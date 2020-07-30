114ea090dde1a57d584d26f78aab71584f349496
nephinj <john.nephin@outlook.com>
2020-07-30 14:47:26 -0400

    fixed local

--- 0-locals.tf
+++ 0-locals.tf
@@ -4,7 +4,7 @@ locals {
   plan            = var.plan == null ? [] : [var.plan]
   boot_diagnostic = var.boot_diagnostic ? ["1"] : []
   unique          = substr(sha1(var.resource_group.id), 0, 8)
-  fixname         = replace(var.vm-name, "-", "")
+  fixname         = replace(local.vm-name, "-", "")
   fixname2        = replace(local.fixname, "_", "")
   fixname3        = substr("${local.fixname2}diag", 0, 16)
   storageName     = lower("${local.fixname3}${local.unique}")
